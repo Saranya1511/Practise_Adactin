88687646f955ece6f3489bdd8b086064ef163e74
Saranya1511 <saranya_1511@yahoo.co.in>
2020-08-25 23:10:54 +0530

    first commit
    push changes for login

---
+++ src/main/java/Practise/Login.java
@@ -0,0 +1,30 @@
+package Practise;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import org.testng.annotations.Test;
+
+
+public class Login 
+{
+
+@Test
+public void Case1() {
+	System.setProperty("webdriver.chrome.driver","/Users/Saranya/Desktop/selenium/chromedriver");
+	WebDriver driver = new ChromeDriver();
+	driver.get("http://adactinhotelapp.com");
+    
+	WebElement user1=driver.findElement(By.id("username"));
+	user1.sendKeys("Saran4Harish");
+	
+	WebElement pass=driver.findElement(By.id("password"));
+	pass.sendKeys("kanishka2!");
+	
+	driver.findElement(By.id("login")).click();
+	
+}
+}
+
